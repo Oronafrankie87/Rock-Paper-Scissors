98ad9d59c14dba29d7a4866b4b420d7ae55ddbf4
Francisco Orona <oronafrankie87@gmail.com>
2023-04-26 14:45:25 -0600

    updates

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+let wins = 0;
+let ties = 0;
+let losses = 0;
+var options = ["Rock", "Paper", "Scissors"];
+
+document.getElementById("startGame").addEventListener("click", function () {
+  var userInput = prompt("Type your choice of Rock, Paper or Scissors");
+  if (!userInput) {
+    return alert("Make a choice fool!");
+  }
+
+  userInput = userInput.toUpperCase();
+
+  var index = Math.floor(Math.random() * options.length);
+  var computerChoice = options[index];
+
+  window.alert("Computer chose " + computerChoice);
+
+  if (userInput === computerChoice) {
+    ties++;
+    window.alert("It's a tie!");
+  } else if (
+    (userInput === "Rock" && computerChoice === "Scissors") ||
+    (userInput === "Paper" && computerChoice === "Rock") ||
+    (userInput === "Scissors" && computerChoice === "Paper")
+  ) {
+    wins++;
+    window.alert("You win!!");
+  } else {
+    losses++;
+    window.alert("Computer wins!!");
+  }
+
+  window.alert(
+    "Stats:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties
+  );
+});
